2be7dd36b6a496ed2ca3a97ca708d259c5f42bbd
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-25 22:53:20 -0400

    Ex1 database and linq query problem

--- MudrakPatel_Lab06_Ex1/Form1.Designer.cs
+++ MudrakPatel_Lab06_Ex1/Form1.Designer.cs
@@ -29,11 +29,123 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.baseballDataSet = new MudrakPatel_Lab06_Ex1.BaseballDataSet();
+            this.playersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.playersTableAdapter = new MudrakPatel_Lab06_Ex1.BaseballDataSetTableAdapters.PlayersTableAdapter();
+            this.playerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.battingAverageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baseballDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.playerIDDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.battingAverageDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.playersBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(452, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // baseballDataSet
+            // 
+            this.baseballDataSet.DataSetName = "BaseballDataSet";
+            this.baseballDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // playersBindingSource
+            // 
+            this.playersBindingSource.DataMember = "Players";
+            this.playersBindingSource.DataSource = this.baseballDataSet;
+            // 
+            // playersTableAdapter
+            // 
+            this.playersTableAdapter.ClearBeforeFill = true;
+            // 
+            // playerIDDataGridViewTextBoxColumn
+            // 
+            this.playerIDDataGridViewTextBoxColumn.DataPropertyName = "PlayerID";
+            this.playerIDDataGridViewTextBoxColumn.HeaderText = "PlayerID";
+            this.playerIDDataGridViewTextBoxColumn.Name = "playerIDDataGridViewTextBoxColumn";
+            this.playerIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            // 
+            // battingAverageDataGridViewTextBoxColumn
+            // 
+            this.battingAverageDataGridViewTextBoxColumn.DataPropertyName = "BattingAverage";
+            this.battingAverageDataGridViewTextBoxColumn.HeaderText = "BattingAverage";
+            this.battingAverageDataGridViewTextBoxColumn.Name = "battingAverageDataGridViewTextBoxColumn";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(13, 13);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(222, 20);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(242, 13);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 261);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baseballDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private BaseballDataSet baseballDataSet;
+        private System.Windows.Forms.BindingSource playersBindingSource;
+        private BaseballDataSetTableAdapters.PlayersTableAdapter playersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn playerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn battingAverageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
     }
 }
 
--- MudrakPatel_Lab06_Ex1/Form1.cs
+++ MudrakPatel_Lab06_Ex1/Form1.cs
@@ -12,9 +12,54 @@ namespace MudrakPatel_Lab06_Ex1
 {
     public partial class Form1 : Form
     {
+        public BaseballDataSet db = new BaseballDataSet();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'baseballDataSet.Players' table. You can move, or remove it, as needed.
+            this.playersTableAdapter.Fill(this.baseballDataSet.Players);
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedPlayer = from player in db.Players.AsEnumerable()
+                                     where (player.FirstName.Equals(searchTextBox.Text)
+                                            || player.LastName.Equals(searchTextBox.Text))
+                                     select player;
+                var details = "";
+                foreach (var detail in selectedPlayer)
+                {
+                    details = details + "<<" + detail.PlayerID + ">> "
+                                      + "<<" + detail.FirstName + ">> "
+                                      + "<<" + detail.LastName + ">> "
+                                      + "<<" + Convert.ToString(detail.BattingAverage) + ">> "
+                                      + Environment.NewLine;
+                }
+                MessageBox.Show(details, "--Searched players--");
+            }
+            catch (FieldAccessException exception)
+            {
+                MessageBox.Show(exception.Message, "--Exception--");
+            }
+            catch (FormatException exception)
+            {
+                MessageBox.Show(exception.Message, "--Exception--");
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                MessageBox.Show(exception.Message, "--Exception--");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "--Exception--");
+            }
         }
     }
 }
