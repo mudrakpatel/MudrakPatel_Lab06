c9dbf5054d42596d599d07302f31e131ac50ad97
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-28 15:29:26 -0400

    Added class ObservableListSource to Ex2 Project

--- MudrakPatel_Lab06_Ex2/DisplayPlayersEx2.Designer.cs
+++ MudrakPatel_Lab06_Ex2/DisplayPlayersEx2.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.rangeLabel = new System.Windows.Forms.Label();
             this.minValueTextBox = new System.Windows.Forms.TextBox();
+            this.toLabel = new System.Windows.Forms.Label();
+            this.maxValueTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // rangeLabel
@@ -49,11 +52,40 @@
             this.minValueTextBox.TabIndex = 1;
             this.minValueTextBox.Text = "0.000";
             // 
+            // toLabel
+            // 
+            this.toLabel.AutoSize = true;
+            this.toLabel.Location = new System.Drawing.Point(281, 46);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(22, 13);
+            this.toLabel.TabIndex = 2;
+            this.toLabel.Text = "TO";
+            // 
+            // maxValueTextBox
+            // 
+            this.maxValueTextBox.Location = new System.Drawing.Point(312, 42);
+            this.maxValueTextBox.Name = "maxValueTextBox";
+            this.maxValueTextBox.Size = new System.Drawing.Size(100, 20);
+            this.maxValueTextBox.TabIndex = 3;
+            this.maxValueTextBox.Text = "1.000";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(424, 40);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
             // DisplayPlayersEx2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 313);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.maxValueTextBox);
+            this.Controls.Add(this.toLabel);
             this.Controls.Add(this.minValueTextBox);
             this.Controls.Add(this.rangeLabel);
             this.Name = "DisplayPlayersEx2";
@@ -67,6 +99,9 @@
 
         private System.Windows.Forms.Label rangeLabel;
         private System.Windows.Forms.TextBox minValueTextBox;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.TextBox maxValueTextBox;
+        private System.Windows.Forms.Button searchButton;
     }
 }
 
