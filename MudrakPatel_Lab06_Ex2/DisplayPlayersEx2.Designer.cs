eadcac16bab94834d96d0efcca5acbe1001049a4
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-28 15:19:12 -0400

    Added Ex2

---
+++ MudrakPatel_Lab06_Ex2/DisplayPlayersEx2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MudrakPatel_Lab06_Ex2
+{
+    partial class DisplayPlayersEx2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rangeLabel = new System.Windows.Forms.Label();
+            this.minValueTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rangeLabel
+            // 
+            this.rangeLabel.AutoSize = true;
+            this.rangeLabel.Location = new System.Drawing.Point(13, 44);
+            this.rangeLabel.Name = "rangeLabel";
+            this.rangeLabel.Size = new System.Drawing.Size(153, 13);
+            this.rangeLabel.TabIndex = 0;
+            this.rangeLabel.Text = "Batting average range input -> ";
+            // 
+            // minValueTextBox
+            // 
+            this.minValueTextBox.Location = new System.Drawing.Point(173, 43);
+            this.minValueTextBox.Name = "minValueTextBox";
+            this.minValueTextBox.Size = new System.Drawing.Size(100, 20);
+            this.minValueTextBox.TabIndex = 1;
+            this.minValueTextBox.Text = "0.000";
+            // 
+            // DisplayPlayersEx2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 313);
+            this.Controls.Add(this.minValueTextBox);
+            this.Controls.Add(this.rangeLabel);
+            this.Name = "DisplayPlayersEx2";
+            this.Text = "Display players";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label rangeLabel;
+        private System.Windows.Forms.TextBox minValueTextBox;
+    }
+}
+
---
+++ MudrakPatel_Lab06_Ex2/DisplayPlayersEx2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MudrakPatel_Lab06_Ex2
+{
+    public partial class DisplayPlayersEx2 : Form
+    {
+        public DisplayPlayersEx2()
+        {
+            InitializeComponent();
+        }
+    }
+}
